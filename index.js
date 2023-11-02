708871eed98d471b06b5caeb6a4b6f8365455cca
Nirmalya Mondal <35608135+nirmalyax@users.noreply.github.com>
2023-11-02 12:36:33 +0530

    add data to mongo db

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ toMongo();
 const app = express();
 const port = 3000;
 
+app.use(express.json())
 //Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const Users = require('../models/Users');
 const router = express.Router()
 
-router.get('/', (req, res)=>{
-    const obj = {
-        a: 'this os',
-        number: 34
-    };
-    res.json(obj);
+
+router.post('/', (req, res)=>{
+    console.log(req.body)
+    const user = Users(req.body)
+    user.save()
+    res.send(req.body);
 });
 
 module.exports = router
