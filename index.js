8f4986f5df29cd90753461b040def3c3f07a2fd9
Nirmalya Mondal <35608135+nirmalyax@users.noreply.github.com>
2023-11-02 12:45:44 +0530

    new

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ toMongo();
 const app = express();
 const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
+
 //Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port "http://localhost:${port}"`);
+  console.log(`App listening on port --> "http://localhost:${port}"`);
 });
-
