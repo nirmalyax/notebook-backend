4df48eed57d10c03ab213f328ea6f50d2feff681
Nirmalya Mondal <35608135+nirmalyax@users.noreply.github.com>
2023-11-02 14:37:51 +0530

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const toMongo = require("./database");
 const express = require("express");
 toMongo();
 const app = express();
-const port = 3000;
+const port = 5001;
 
 app.use(express.json());
 
--- routes/auth.js
+++ routes/auth.js
@@ -4,34 +4,42 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator");
 
 router.post(
-  "/",
+  "/createnewuser",
   [
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("username", "Enter a valid username").isLength({ min: 5 }),
     body("email", "Enter a valid Email").isEmail(),
-    body("password", "Password must have a minimum of 5 characters").isLength({
+    body("password", "Enter a valid Password").isLength({
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
-      return res
-        .status(400)
-        .json({ errors: errors.array("please input valid data") });
+      return res.status(400).json({ errors: errors.array.json("") });
     }
 
-    Users.create({
-      name: req.body.name,
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({ error: "please provide unique username and email"});
+    try {
+      let user = await Users.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "email already exists" });
+      }
+      user = await Users.findOne({ username: req.body.username });
+      if (user) {
+        return res.status(400).json({ error: "username already exists" });
+      }
+      user = await Users.create({
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
       });
+      res.json({ Database: "Updated" });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Error");
+    }
   }
 );
 
